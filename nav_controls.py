cc6a556c71c824a1b48bbd50bdc0bb9a342cc7fd
Grady-1028 <104150276+Grady-1028@users.noreply.github.com>
2022-04-29 09:39:27 -0400

    Update nav_controls.py

--- nav_controls.py
+++ nav_controls.py
@@ -1,4 +1,6 @@
-#Keybrd Nav Cntrls
+# nav controls for microbit
+
+from microbit import *
 from cyberbot import *
 
 sleep(1000)
@@ -6,14 +8,19 @@ sleep(1000)
 print("\nSpeeds are -100 to 100\n")
 
 while(True):
-    vL = int(input("Enter left speed: "))
-    vR = int(input("Enter right speed: "))
-    ms = int(input("Enter ms to run: "))
+    try:
+        vL = int(input("Enter left speed: "))
+        vR = int(input("Enter right speed: "))
+        ms = int(input("Enter ms to run: "))
+    
+        bot(18).servo_speed(vL)
+        bot(19).servo_speed(-vR)
+        sleep(ms)
+        bot(18).servo_speed(None)
+        bot(19).servo_speed(None)
+         
+        print() 
 
-    bot(18).servo_speed(vL)
-    bot(19).servo_speed(-vR)
-    sleep(ms)
-    bot(18).servo_speed(None)
-    bot(19).servo_speed(None)
-
-    print() 
+    except:
+        print("Oh no! An Error: Unaccepted Value \n")
+        print("Please, try again. \n")
